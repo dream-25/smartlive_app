6996c238e32e2cd60e79c23413219290604dd8ed
dream-25 <biswasmohan2529@gmail.com>
2022-04-25 22:37:17 +0530

    @ItsMohan025   04/25/2022  22:37:17.00 PM

---
+++ android/app/src/main/kotlin/com/dreamapp/smartlive/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dreamapp.smartlive
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ deploy.cmd
@@ -0,0 +1,9 @@
+cls
+cd C:\Users\Dream-25\Desktop\LIQUID EXCHANGE\liquid_exchange
+for /F "tokens=2" %%i in ('date /t') do set mydate=%%i
+for /F "tokens=2" %%i in ('time /t') do set mytimeam=%%i
+set mytime=%time%
+git add .
+git commit . -m "@ItsMohan025   %mydate%  %mytime% %mytimeam%"
+git push -f origin main
+echo "Deploy Done"
